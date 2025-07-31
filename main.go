240c0f4f131ebb289b3c5b20bca1931012a0296c
Nerein <nerein878@gmail.com>
2025-07-31 15:41:53 +0300

    add struct Page and method Save

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("My first code on GitHub")
+}
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (P *Page) save() error {
+	filename := P.Title + ".txt"
+	return os.WriteFile(filename, P.Body, 0644)
 }
